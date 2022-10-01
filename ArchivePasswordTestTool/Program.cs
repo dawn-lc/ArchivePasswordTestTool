2ec97a468be3ef354be63526432ac31a5d8f959c
dawn-lc <lc990930@163.com>
2022-10-02 01:42:59 +0800

    重写

---
+++ ArchivePasswordTestTool/Program.cs
@@ -0,0 +1,214 @@
+﻿using Sentry;
+using SevenZip;
+using Spectre.Console;
+using System.Diagnostics;
+using System.Net;
+using System.Text.Json;
+using static ArchivePasswordTestTool.Utils;
+using static ArchivePasswordTestTool.Utils.Util;
+
+namespace ArchivePasswordTestTool
+{
+    public class Program
+    {
+        public static readonly string AppName = "ArchivePasswordTestTool";
+        public static readonly int[] Version = new int[] { 1, 5, 1 };
+        public static readonly string VersionType = "Release";
+        public static readonly string AppHomePage = "https://www.bilibili.com/read/cv6101558";
+        public static readonly string Developer = "dawn-lc";
+        public static Config config { get; set; }
+        public class Lib
+        {
+            public string Name { get; set; }
+            public string Hash { get; set; }
+            public string DownloadUrl { get; set; }
+            public bool Exists { get; set; }
+        }
+
+        public class Config
+        {
+            public DateTime CheckUpgrade { get; set; } = new();
+            public List<Lib> Libs { get; set; } = new();
+            public string Dictionary { get; set; } = "PasswordDictionary.txt";
+        }
+
+        private static async Task Initialization(StatusContext ctx)
+        {
+            ctx.Status("加载配置文件...");
+            config = await DeserializeJSONFileAsync<Config>("config.json");
+            Log($"上次检查更新:{config.CheckUpgrade.ToLocalTime()}");
+            if (config.CheckUpgrade < (DateTime.Now - new TimeSpan(1, 0, 0, 0)))
+            {
+                Log("正在从github.com获取最新版本信息...");
+                try
+                {
+                    HttpResponseMessage Info = await HTTP.GetAsync(new Uri($"https://api.github.com/repos/{Developer}/{AppName}/releases/latest"), new Dictionary<string, IEnumerable<string>>() { ["user-agent"] = new List<string>() { AppName + " " + string.Join(".", Version) } });
+                    if (Info.StatusCode != HttpStatusCode.OK)
+                    {
+                        Error("检查更新失败！请检查您的网络情况。");
+                        throw new Exception($"检查更新失败！\r\n{ Info.StatusCode }\r\n{ await Info.Content.ReadAsStringAsync() }");
+                    }
+                    else
+                    {
+                        Upgrade.ReleasesInfo? LatestInfo = JsonSerializer.Deserialize<Upgrade.ReleasesInfo>(await Info.Content.ReadAsStringAsync());
+                        if (LatestInfo != null)
+                        {
+                            if (Upgrade.CheckUpgrade(LatestInfo, Version, VersionType))
+                            {
+                                Log("当前本地程序已是最新版本。");
+                                config.CheckUpgrade = DateTime.Now;
+                                config.Libs.Clear();
+                                foreach (var item in (LatestInfo.Body ?? "").Split("\r\n"))
+                                {
+                                    if (!string.IsNullOrEmpty(item) && item[0..4] == "lib." && LatestInfo.Assets.Any(i => i.Name == item.Split(":")[0]))
+                                    {
+                                        LatestInfo.Assets.Find(i => i.Name == item.Split(":")[0]).Label = item.Split(":")[1];
+                                    }
+                                }
+                                foreach (var item in LatestInfo.Assets)
+                                {
+                                    if (item.Name.Contains("lib."))
+                                    {
+                                        config.Libs.Add(new() { Name = item.Name.Replace("lib.", ""), DownloadUrl = item.DownloadUrl, Hash = item.Label });
+                                    }
+                                }
+                                File.WriteAllText($"config.json", JsonSerializer.Serialize(config));
+                            }
+                            else
+                            {
+                                Warn("版本不是最新的, 请下载最新版。");
+                                Process.Start("explorer.exe", AppHomePage);
+                                throw new Exception("版本过低！");
+                            }
+                        }
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    Error("检查更新失败！请检查您的网络情况。");
+                    throw new Exception($"检查更新失败！\r\n {ex}");
+                }
+            }
+            ctx.Status("正在检查运行环境...");
+            if (!Directory.Exists("lib"))
+            {
+                Directory.CreateDirectory("lib");
+            }
+            foreach (var item in config.Libs)
+            {
+                if (File.Exists($"lib/{item.Name}"))
+                {
+                    using var file = File.OpenRead($"lib/{item.Name}");
+                    if (ComparisonFileHash(file, Convert.FromBase64String(item.Hash)))
+                    {
+                        Log($"{item.Name} 校验成功 ");
+                        item.Exists = true;
+                        continue;
+                    }
+                }
+                Warn($"{item.Name} 运行库缺少或损坏! ");
+                item.Exists = false;
+            }
+            if (!File.Exists("PasswordDictionary.txt"))
+            {
+                File.WriteAllText("PasswordDictionary.txt", "");
+            }
+        }
+        static async Task Main(string[] args)
+        {
+            using (SentrySdk.Init(o =>
+            {
+                o.Dsn = "https://9361b53d22da420c95bdb43d1b78eb1e@o687854.ingest.sentry.io/5773141";
+                o.TracesSampleRate = 1.0;
+            }))
+            {
+                string ArchiveFile;
+                await AnsiConsole.Status().StartAsync("初始化...", async ctx => {
+                    await Initialization(ctx);
+                });
+                if (config.Libs.Any(i => !i.Exists))
+                {
+                    await AnsiConsole.Progress().AutoClear(true).HideCompleted(true).StartAsync(async ctx =>
+                    {
+                        foreach (var item in config.Libs.Where(i => !i.Exists))
+                        {
+                            HTTP.DownloadAsync(await HTTP.GetAsync(new Uri(item.DownloadUrl), new Dictionary<string, IEnumerable<string>>() { ["user-agent"] = new List<string>() { AppName + " " + string.Join(".", Version) } }), $"lib/{item.Name}", ctx.AddTask($"下载 {item.Name}"), item.Name);
+                        }
+                    });
+                    Log("下载完成，请重启软件以完成更新。");
+                    await Task.Delay(5000);
+                    Environment.Exit(0);
+                }
+                AnsiConsole.Clear();
+
+                while (true)
+                {
+                    if (StartupParametersCheck(args, "D"))
+                    {
+                        config.Dictionary = GetParameter(args, "D", "PasswordDictionary.txt");
+                    }
+                    else
+                    {
+                        config.Dictionary = AnsiConsole.Ask("请输入需要进行测试的密码字典路径[[或将密码字典拖至本窗口]]", "PasswordDictionary.txt");
+                    }
+
+                    if (File.Exists(config.Dictionary))
+                    {
+                        break;
+                    }
+                }
+                while (true)
+                {
+                    if (StartupParametersCheck(args, "F"))
+                    {
+                        ArchiveFile = GetParameter(args, "F", AnsiConsole.Ask<string>("请输入需要进行测试的压缩包路径[[或将压缩包拖至本窗口]]:"));
+                    }
+                    else
+                    {
+                        ArchiveFile = AnsiConsole.Ask<string>("请输入需要进行测试的压缩包路径[[或将压缩包拖至本窗口]]:");
+                    }
+                    if (File.Exists(ArchiveFile))
+                    {
+                        break;
+                    }
+                }
+
+                SevenZipBase.SetLibraryPath("lib/7z.dll");
+                string? EncryptArchivePassword = null;
+                await AnsiConsole.Status().StartAsync("测试密码中...", async ctx => {
+                    Parallel.ForEach(File.ReadAllLines(config.Dictionary), new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount - 1 }, (i, loopState) =>
+                    {
+                        using var temp = new SevenZipExtractor(ArchiveFile, i);
+                        if (temp.Check())
+                        {
+                            EncryptArchivePassword = i;
+                            loopState.Break();
+                        }
+                    });
+                    ctx.Status("测试结束。");
+                    await Task.Delay(500);
+                });
+                AnsiConsole.WriteLine(EncryptArchivePassword != null ? $"已找到解压密码: {EncryptArchivePassword}" : "没有找到正确的解压密码！");
+                if (AnsiConsole.Confirm("是否保存测试结果?", true))
+                {
+                    using (StreamWriter file = new($"{ArchiveFile}[测试报告].txt", false))
+                    {
+                        file.WriteLine("加密压缩包: " + ArchiveFile);
+                        file.WriteLine("字典: " + config.Dictionary);
+                        if (EncryptArchivePassword != null)
+                        {
+                            file.WriteLine("解压密码: " + EncryptArchivePassword);
+                        }
+                        else
+                        {
+                            file.WriteLine("没有找到正确的解压密码！");
+                        }
+                        file.Close();
+                    }
+                    Process.Start("Explorer.exe", $"/select, \"{ArchiveFile}[测试报告].txt\"");
+                }
+            }
+        }
+    }
+}
---
+++ ArchivePasswordTestTool/Utils.cs
@@ -0,0 +1,369 @@
+﻿using Spectre.Console;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static ArchivePasswordTestTool.Utils.Util;
+
+namespace ArchivePasswordTestTool
+{
+    internal class Utils
+    {
+        public static class Util
+        {
+            /// <summary>
+            /// 比较两个字节数组是否相等
+            /// </summary>
+            /// <param name="A">byte数组1</param>
+            /// <param name="B">byte数组2</param>
+            /// <returns>是否相等</returns>
+            public static bool Equals(byte[] A, byte[] B)
+            {
+                if (A == null || B == null) return false;
+                if (A.Length != B.Length) return false;
+                for (int i = 0; i < A.Length; i++)
+                    if (A[i] != B[i])
+                        return false;
+                return true;
+            }
+            /// <summary>
+            /// 比较文件Hash是否一致
+            /// </summary>
+            /// <param name="File">文件流</param>
+            /// <param name="Hash">文件流</param>
+            /// <returns>是否一致</returns>
+            public static bool ComparisonFileHash(Stream File, byte[] Hash)
+            {
+                try
+                {
+                    using MD5 FileMD5 = MD5.Create();
+                    return Equals(FileMD5.ComputeHash(File), Hash);
+                }
+                catch (Exception ex)
+                {
+                    Error(ex.ToString());
+                    return false;
+                }
+            }
+            /// <summary>
+            /// 比较两个文件是否一致
+            /// </summary>
+            /// <param name="FileA">文件流</param>
+            /// <param name="FileB">文件流</param>
+            /// <returns>是否一致</returns>
+            public static bool ComparisonFile(Stream FileA, Stream FileB)
+            {
+                try
+                {
+                    using MD5 FileMD5 = MD5.Create();
+                    //Log(Convert.ToBase64String(FileMD5.ComputeHash(FileA)));
+                    return Equals(FileMD5.ComputeHash(FileA), FileMD5.ComputeHash(FileB));
+                }
+                catch (Exception ex)
+                {
+                    Error(ex.ToString());
+                    return false;
+                }
+            }
+            /// <summary> 从JSON文件反序列化对象 </summary>
+            public static async Task<T> DeserializeJSONFileAsync<T>(string path) where T : new()
+            {
+                T? data;
+                return File.Exists(path) ? (data = JsonSerializer.Deserialize<T>(await File.ReadAllTextAsync(path))) != null ? data : new T() : new T();
+            }
+            /// <summary> 从JSON文件反序列化对象 </summary>
+            public static T DeserializeJSONFile<T>(string path) where T : new()
+            {
+                T? data;
+                return File.Exists(path) ? (data = JsonSerializer.Deserialize<T>(File.ReadAllText(path))) != null ? data : new T() : new T();
+            }
+            public static void Log(string value)
+            {
+                AnsiConsole.MarkupLine($"[bold][[{DateTime.Now}]] [lime]Info[/][/] {value}");
+            }
+            public static void Warn(string value)
+            {
+                AnsiConsole.MarkupLine($"[bold][[{DateTime.Now}]] [orangered1]Warning[/][/] {value}");
+            }
+            public static void Error(string value)
+            {
+                AnsiConsole.MarkupLine($"[bold][[{DateTime.Now}]] [red]Error[/][/] {value}");
+            }
+            public static bool StartupParametersCheck(List<string> Parameters, string Flag)
+            {
+                if (Parameters.Contains($"-{Flag}"))
+                {
+                    try
+                    {
+                        return !string.IsNullOrEmpty(GetParameter(Parameters, Flag, ""));
+                    }
+                    catch (Exception)
+                    {
+                        throw new Exception($"Startup parameters error. Please check: {Flag}");
+                    }
+                }
+                return false;
+            }
+            public static bool StartupParametersCheck(string[] Parameters, string Flag)
+            {
+                return StartupParametersCheck(new List<string>(Parameters), Flag);
+            }
+            public static T GetParameter<T>(List<string> Parameters, string Flag, T DefaultParameter)
+            {
+                return (T)Convert.ChangeType(Parameters[Parameters.IndexOf($"-{Flag}") + 1], typeof(T)) ?? DefaultParameter;
+            }
+            public static T GetParameter<T>(string[] Parameters, string Flag, T DefaultParameter)
+            {
+                return GetParameter<T>(new List<string>(Parameters), Flag, DefaultParameter);
+            }
+            public static T? GetParameter<T>(List<string> Parameters, string Flag)
+            {
+                return (T)Convert.ChangeType(Parameters[Parameters.IndexOf($"-{Flag}") + 1], typeof(T));
+            }
+            public static T? GetParameter<T>(string[] Parameters, string Flag)
+            {
+                return GetParameter<T>(new List<string>(Parameters), Flag);
+            }
+
+        }
+        public static class Upgrade
+        {
+            public class ReleasesInfo
+            {
+                public class Asset
+                {
+                    [JsonPropertyName("name")]
+                    public string Name { get; set; }
+                    [JsonPropertyName("label")]
+                    public string Label { get; set; }
+                    [JsonPropertyName("content_type")]
+                    public string ContentType { get; set; }
+                    [JsonPropertyName("created_at")]
+                    public string CreatedAt { get; set; }
+                    [JsonPropertyName("updated_at")]
+                    public string UpdatedAt { get; set; }
+                    [JsonPropertyName("browser_download_url")]
+                    public string DownloadUrl { get; set; }
+                }
+
+                [JsonPropertyName("tag_name")]
+                public string TagName { get; set; }
+                [JsonPropertyName("target_commitish")]
+                public string TargetCommitish { get; set; }
+                [JsonPropertyName("name")]
+                public string Name { get; set; }
+                [JsonPropertyName("prerelease")]
+                public bool Prerelease { get; set; }
+                [JsonPropertyName("created_at")]
+                public string CreatedAt { get; set; }
+                [JsonPropertyName("published_at")]
+                public string PublishedAt { get; set; }
+                [JsonPropertyName("assets")]
+                public List<Asset> Assets { get; set; }
+                [JsonPropertyName("body")]
+                public string? Body { get; set; }
+            }
+            /// <summary>
+            /// 对比版本号
+            /// </summary>
+            /// <param name="sourceVersion">源版本</param>
+            /// <param name="targetVersion">目标版本</param>
+            /// <returns><see langword="1"/> 目标版本类型较高<br /><see langword="0"/> 两者版本类型一致<br /><see langword="-1"/> 源版本类型较高</returns> 
+            /// <exception cref="ArgumentException"></exception>
+            private static int ComparisonVersion(int[] sourceVersion, int[] targetVersion)
+            {
+                if (sourceVersion.Length == targetVersion.Length)
+                {
+                    for (int i = 0; i < sourceVersion.Length; i++)
+                    {
+                        if (sourceVersion[i] != targetVersion[i])
+                        {
+                            if (sourceVersion[i] < targetVersion[i])
+                            {
+                                return 1;
+                            }
+                            else
+                            {
+                                return -1;
+                            }
+                        }
+                    }
+                    return 0;
+                }
+                else
+                {
+                    throw new ArgumentException("版本号格式不一致");
+                }
+
+            }
+            /// <summary>
+            /// 对比版本类型
+            /// </summary>
+            /// <param name="sourceVersion">源版本类型</param>
+            /// <param name="targetVersion">目标版本类型</param>
+            /// <returns><see langword="1"/> 目标版本类型较高<br /><see langword="0"/> 两者版本类型一致<br /><see langword="-1"/> 源版本类型较高</returns> 
+            /// <exception cref="ArgumentException"></exception>
+            private static int ComparisonVersionType(string sourceVersionType, string targetVersionType)
+            {
+                List<List<string>> VersionType = new()
+                {
+                    new List<string> { "fixpush" },
+                    new List<string> { "final", "full version", "enhance", "standard" },
+                    new List<string> { "release", "release candidate" },
+                    new List<string> { "preview" },
+                    new List<string> { "beta" },
+                    new List<string> { "alpha" },
+                    new List<string> { "free", "demo", "test" }
+                };
+
+                if (VersionType.Where(p => p.Contains(sourceVersionType.ToLower())).Any() && VersionType.Where(p => p.Contains(targetVersionType.ToLower())).Any())
+                {
+                    int sourceVersionTypeLevel = VersionType.Count;
+                    int targetVersionLevel = VersionType.Count;
+                    for (int i = 1; i < VersionType.Count; i++)
+                    {
+                        if (VersionType[i].Contains(sourceVersionType.ToLower()))
+                        {
+                            sourceVersionTypeLevel = VersionType.Count - i;
+                        }
+                        if (VersionType[i].Contains(targetVersionType.ToLower()))
+                        {
+                            targetVersionLevel = VersionType.Count - i;
+                        }
+                    }
+
+                    if (sourceVersionTypeLevel < targetVersionLevel)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        if (sourceVersionTypeLevel == targetVersionLevel)
+                        {
+                            return 0;
+                        }
+                        else
+                        {
+                            return -1;
+                        }
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("版本类型无法识别");
+                }
+            }
+            public static bool CheckUpgrade(ReleasesInfo LatestInfo, int[] version, string versionType)
+            {
+                try
+                {
+                    List<string> LatestVersionData = new(LatestInfo.TagName.Split('-'));
+                    List<int> LatestVersion = new();
+                    string LatestVersionType = LatestVersionData[1];
+                    foreach (var item in LatestVersionData[0].Split('.'))
+                    {
+                        LatestVersion.Add(Convert.ToInt32(item));
+                    }
+                    switch (ComparisonVersion(version, LatestVersion.ToArray()))
+                    {
+                        case 1:
+                            break;
+                        case 0:
+                            switch (ComparisonVersionType(versionType, LatestVersionType))
+                            {
+                                case 1:
+                                    break;
+                                case 0:
+                                    return true;
+                                case -1:
+                                    return true;
+                            }
+                            break;
+                        case -1:
+                            return true;
+                    }
+                    Warn("当前版本不是最新的，请前往下载最新版本。");
+                    return false;
+                }
+                catch (Exception)
+                {
+                    Error("检查更新失败！请联系开发者进行修复。");
+                    throw new Exception("无法解析的版本信息格式! \r\n" + LatestInfo.ToString());
+                }
+            }
+        }
+        public static class HTTP
+        {
+            public static HttpRequestMessage CreateRequest(Uri url, HttpMethod method, HttpContent? content)
+            {
+                return new HttpRequestMessage()
+                {
+                    RequestUri = url,
+                    Method = method,
+                    Content = content,
+                };
+            }
+            private static readonly TimeSpan DefaultTimeout = new(0, 0, 1, 0);
+            public static HttpClient Constructor(Dictionary<string, IEnumerable<string>>? head, TimeSpan? timeout)
+            {
+                HttpClientHandler clientHandler = new() { 
+                    AutomaticDecompression = DecompressionMethods.GZip
+                };
+                clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+                HttpClient Client = new(clientHandler);
+                Client.DefaultRequestHeaders.Clear();
+                Client.Timeout = timeout ?? DefaultTimeout;
+                foreach (KeyValuePair<string, IEnumerable<string>> item in head ?? new())
+                {
+                    Client.DefaultRequestHeaders.Add(item.Key, item.Value);
+                }
+                return Client;
+            }
+            public static HttpClient Constructor(Dictionary<string, string>? head = null, TimeSpan? timeout = null)
+            {
+                var heads = new Dictionary<string, IEnumerable<string>>();
+                foreach (var item in head ?? new())
+                {
+                    heads.Add(item.Key, new List<string>(item.Value.Split(';')));
+                }
+                return Constructor(heads, timeout ?? DefaultTimeout);
+            }
+            public static async Task<HttpResponseMessage> GetAsync(Uri url, Dictionary<string, IEnumerable<string>>? head = null, TimeSpan? timeout = null)
+            {
+                return await Constructor(head, timeout).GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+            }
+            public static async Task<HttpResponseMessage> PostAsync(Uri url, HttpContent content, Dictionary<string, IEnumerable<string>>? head = null, TimeSpan? timeout = null)
+            {
+                return await Constructor(head, timeout).PostAsync(url, content);
+            }
+            public static async Task<HttpResponseMessage> PatchAsync(Uri url, HttpContent content, Dictionary<string, IEnumerable<string>>? head = null, TimeSpan? timeout = null)
+            {
+                return await Constructor(head, timeout).PatchAsync(url, content);
+            }
+            public static async Task<HttpResponseMessage> PutAsync(Uri url, HttpContent content, Dictionary<string, IEnumerable<string>>? head = null, TimeSpan? timeout = null)
+            {
+                return await Constructor(head, timeout).PutAsync(url, content);
+            }
+            public static async void DownloadAsync(HttpResponseMessage response, string path, ProgressTask task, string? name)
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                byte[] buffer = new byte[8192];
+                Stream ResponseStream = await response.Content.ReadAsStreamAsync();
+                using var destination = new FileStream(path, FileMode.OpenOrCreate);
+                long contentLength = response.Content.Headers.ContentLength ?? 0;
+                int bytesRead;
+                while ((bytesRead = await ResponseStream.ReadAsync(buffer).ConfigureAwait(false)) != 0)
+                {
+                    await destination.WriteAsync(buffer.AsMemory(0, bytesRead)).ConfigureAwait(false);
+                    task.Increment((double)bytesRead / contentLength * 100);
+                }
+                task.Increment(100);
+                task.StopTask();
+                Log($"{name} 下载完成");
+            }
+        }
+    }
+}
